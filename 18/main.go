7af3a9bbe6e72ea13cb9cc7a9285ddc998acc5a7
Slijkhuis <slijkhuis.thijs@gmail.com>
2024-12-18 09:34:07 +0100

    day 18 part 2

--- 18/main.go
+++ 18/main.go
@@ -89,7 +89,75 @@ func part1() {
 }
 
 func part2() {
-	for line := range aoc.LinesFromFile(os.Args[2]) {
-		fmt.Println(line)
+	width := 6 + 1
+	height := 6 + 1
+	skipCheckUntilByte := 12
+	if !aoc.DebugMode {
+		width = 70 + 1
+		height = 70 + 1
+		skipCheckUntilByte = 1024
 	}
+
+	grid := aoc.NewGridWithDefaultValue(width, height, '.')
+
+	var byteCoords []aoc.Point
+	for line := range aoc.LinesFromFile(os.Args[2]) {
+		coords := aoc.IntsFromString(line)
+		byteCoords = append(byteCoords, aoc.Point{X: coords[0], Y: coords[1]})
+	}
+
+	for i := range skipCheckUntilByte + 1 {
+		grid.Data[byteCoords[i]] = '#'
+	}
+
+	g := aoc.NewGraph[aoc.Point, rune]()
+
+	visited := make(map[aoc.Point]struct{})
+	var buildGraph func(*aoc.Node[aoc.Point, rune])
+	buildGraph = func(comingFrom *aoc.Node[aoc.Point, rune]) {
+		if _, ok := visited[comingFrom.Key]; ok {
+			return
+		}
+		visited[comingFrom.Key] = struct{}{}
+
+		for _, dir := range aoc.NoDiagonals {
+			next := comingFrom.Key.Move(dir)
+			val := grid.Data[next]
+			if val == '.' {
+				n := g.NewOrExistingNode(next, val)
+				g.AddNodeIgnoreDuplicate(n)
+				g.AddWeightedEdge(comingFrom, n, 1)
+				buildGraph(n)
+			}
+		}
+	}
+
+	startNode := aoc.NewNode(aoc.Point{X: 0, Y: 0}, '.')
+	g.AddNode(startNode)
+	buildGraph(startNode)
+
+	endNode := g.Nodes[aoc.Point{X: width - 1, Y: height - 1}]
+
+	index := skipCheckUntilByte + 1
+	for g.AreConnected(startNode, endNode) {
+		index++
+
+		if index >= len(byteCoords) {
+			panic("something is badly wrong")
+		}
+
+		fmt.Println(index, "/", len(byteCoords))
+
+		grid.Data[byteCoords[index]] = '#'
+		for edge := range g.Edges {
+			if edge.From.Key == byteCoords[index] || edge.To.Key == byteCoords[index] {
+				delete(g.Edges, edge)
+			}
+		}
+	}
+
+	aoc.Debug(grid.String())
+
+	// it's very slow... but it does work
+	fmt.Println(byteCoords[index])
 }
