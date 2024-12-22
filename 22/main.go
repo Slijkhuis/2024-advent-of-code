b9b4cc35f9dc03d0280aac0b10eb3c0ddb9dee9c
Slijkhuis <slijkhuis.thijs@gmail.com>
2024-12-22 11:29:26 +0100

    day 22 part 2

--- 22/main.go
+++ 22/main.go
@@ -53,7 +53,72 @@ func generateSecretNumber(n int) int {
 }
 
 func part2() {
+	t := time.Now()
+
+	var initialNumbers []int
 	for line := range aoc.LinesFromFile(os.Args[2]) {
-		fmt.Println(line)
+		initialNumbers = append(initialNumbers, aoc.Atoi(line))
 	}
+
+	numbers := make([][]int, len(initialNumbers))
+	lastDigits := make([][]int, len(initialNumbers))
+	changes := make([][]int, len(initialNumbers))
+
+	var allKeys [][4]int
+	pricePer4Changes := make([]map[[4]int]int, len(initialNumbers))
+
+	for i, n := range initialNumbers {
+		numbers[i] = make([]int, 2000)
+		lastDigits[i] = make([]int, 2000)
+		pricePer4Changes[i] = make(map[[4]int]int)
+
+		for j := range 2000 {
+			n = generateSecretNumber(n)
+			numbers[i][j] = n
+			lastDigits[i][j] = n % 10
+			if j > 0 {
+				changes[i] = append(changes[i], lastDigits[i][j]-lastDigits[i][j-1])
+			}
+			cs := changes[i]
+			lcs := len(cs)
+			if len(cs) >= 4 {
+				key := [4]int{cs[lcs-4], cs[lcs-3], cs[lcs-2], cs[lcs-1]}
+
+				// TODO: not sure if I have to overwrite the value if the newer occurrence has a higher price? Reading the
+				// puzzle text I don't think so, but I need to double check.
+				if _, ok := pricePer4Changes[i][key]; !ok {
+					pricePer4Changes[i][key] = lastDigits[i][j]
+				}
+
+				allKeys = append(allKeys, key)
+			}
+		}
+	}
+
+	aoc.Println(t, "processed all changes")
+
+	allUniqueKeys := aoc.Unique(allKeys)
+	aoc.Println(t, "processed unique keys, went from", len(allKeys), "to", len(allUniqueKeys))
+
+	// Find the best key.
+	var highestKey [4]int
+	var highestValue int
+	for i, key := range allUniqueKeys {
+		if (i+1)%10_000 == 0 {
+			aoc.Println(t, "processing", i+1, "out of", len(allUniqueKeys))
+		}
+
+		var totalPrice int
+		for i := range len(pricePer4Changes) {
+			totalPrice += pricePer4Changes[i][key]
+		}
+
+		if totalPrice > highestValue {
+			highestKey = key
+			highestValue = totalPrice
+		}
+	}
+
+	aoc.Debug(highestKey)
+	aoc.Println(t, highestValue)
 }
